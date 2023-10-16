7ecdba2ed4be55b440d4a8d610e4f5a4a2d64b44
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-10-16 14:32:31 -0300

    =Commit inicial do tio

---
+++ par e impar.py
@@ -0,0 +1,12 @@
+#Apresentação
+print('\n\t\t\t -- Verificador de Números -- \n\n')
+
+#Entradas
+nmr = int(input('digite um numero inteiro e eu lhe direi se é par ou impar:\n'))
+
+#Processamento e Saída
+if (nmr % 2) == 0:
+    print("É par")
+else:
+    print("é impar")
+
